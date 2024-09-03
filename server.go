238b542aeb6787ac64834aa15abb640e931d5a25
log101 <ffrknerdm@gmail.com>
2024-09-03 11:26:12 +0300

    feat: add recover middleware

--- server.go
+++ server.go
@@ -9,6 +9,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/joho/godotenv"
 )
 
@@ -29,6 +30,7 @@ func main() {
 	DB.InitDB()
 
 	app := fiber.New()
+	app.Use(recover.New())
 	app.Use(logger.New())
 	app.Use(cors.New(cors.Config{
 		AllowOrigins: "http://localhost:4321",
